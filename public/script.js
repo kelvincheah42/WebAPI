4128184c3d2d202ef36ce16f10b6d37adde7409d
Kelvin Cheah Veen Yee <kelvincheah42@gmail.com>
2017-07-19 13:04:30 +0000

    Initial commit

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-// script.js
-// create the module and name it travelWeb & include ngRoute for all routing needs
     var travelWeb = angular.module('travelWeb', ['ngRoute']);
 
     // configure our routes
@@ -11,18 +9,6 @@
                 templateUrl : 'index.html',
                 controller  : 'Ctrl'
             })
-
-            // route for the about page
-            .when('/favourite', {
-                templateUrl : 'favourite.html',
-                controller  : 'favouriteController'
-            })
-
-            // route for the contact page
-            .when('/contact', {
-                templateUrl : 'contact.html',
-                controller  : 'contactController'
-            })
             
             .otherwise({
                 redirectTo: 'index'
@@ -32,13 +18,7 @@
     // create the controller and display Angular's $scope
     travelWeb.controller('Ctrl', function($scope, $window) {
         // create message to display in the view
-        $scope.message = 'Use search bar below to search places';
+        $scope.message = 'TESTING 123';
     });
 
-    travelWeb.controller('favouriteController', function($scope) {
-        $scope.message = 'Favourite places at below';
-    });
-
-    travelWeb.controller('contactController', function($scope) {
-        $scope.message = 'Drop us message for any question!';
-    });
+  
